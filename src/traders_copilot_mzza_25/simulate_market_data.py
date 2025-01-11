abce10c15dc2fa250c548e45e68d77d8fa8f5fb2
Zanan Pech <zanan@student.ubc.ca>
2025-01-10 22:19:20 -0800

    refactor: rename fetch_market_data to simulate_market_data

---
+++ src/traders_copilot_mzza_25/simulate_market_data.py
@@ -0,0 +1,14 @@
+def simulate_market_data(start_date):
+    """Simulate stock market data that contains date and price columns
+    from start date until today.
+
+
+    Parameters:
+    ----------
+    start_date: Start date of the simulation (YYYY-MM-DD).
+
+    Returns:
+    ----------
+    pd.DataFrame
+        The dataframe that contains columns for stock market data.
+    """
